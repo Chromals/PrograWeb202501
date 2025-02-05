ee15641eb59e64c9790f84be54cd79807cb70bcb
Pablo Ramos <juanpabloramosp@hotmail.com>
2025-02-04 18:40:31 -0600

    Conf Serilog completed

--- Northwind202501/BackEnd/Program.cs
+++ Northwind202501/BackEnd/Program.cs
@@ -3,6 +3,7 @@ using BackEnd.Services.Interfaces;
 using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+#region Serilog
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog((ctx,lc   )=> lc
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day) 
+    .MinimumLevel.Information()
+    );  
+
+#endregion
 
 #region DI
 builder.Services.AddDbContext<NorthWindContext>();
