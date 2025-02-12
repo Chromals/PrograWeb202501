62adf19aebf97f03148e540619f22029360c5d1d
Pablo Ramos <juanpabloramosp@hotmail.com>
2025-02-11 18:59:15 -0600

    Service Completed

--- Northwind202501/BackEnd/Services/Interfaces/ISupplierService.cs
+++ Northwind202501/BackEnd/Services/Interfaces/ISupplierService.cs
@@ -1,6 +1,17 @@
-﻿namespace BackEnd.Services.Interfaces
+﻿using BackEnd.DTO;
+
+namespace BackEnd.Services.Interfaces
 {
     public interface ISupplierService
     {
+        SupplierDTO Get();
+        List<SupplierDTO> GetAll();
+
+        SupplierDTO Add(SupplierDTO supplierDTO);
+        SupplierDTO Update(SupplierDTO supplierDTO);
+        SupplierDTO Delete(int id);
+
+
+
     }
 }
