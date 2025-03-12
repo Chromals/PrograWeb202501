3030489682a6f87dddc7e60cb87e12dab1a1dd66
Pablo Ramos <juanpabloramosp@hotmail.com>
2025-03-11 18:41:18 -0600

    Config basica Identity

--- Northwind202501/BackEnd/Program.cs
+++ Northwind202501/BackEnd/Program.cs
@@ -3,6 +3,7 @@ using BackEnd.Services.Interfaces;
 using DAL.Implementations;
 using DAL.Interfaces;
 using Entities.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -22,16 +23,45 @@ builder.Logging.ClearProviders();
 builder.Host.UseSerilog((ctx,lc   )=> lc
     .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day) 
     .MinimumLevel.Error()
-    );  
+    );
 
 #endregion
 
-#region DI
-builder.Services.AddDbContext<NorthWindContext>(optionsAction => 
+#region BD
+builder.Services.AddDbContext<NorthWindContext>(optionsAction =>
                     optionsAction
                     .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 
     );
+
+builder.Services.AddDbContext<AuthDBContext>(optionsAction =>
+                    optionsAction
+                    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+
+    );
+
+#endregion
+
+
+#region Identity
+
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<AuthDBContext>()
+        .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+   options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+
+});
+#endregion
+
+#region DI
+
 builder.Services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICategoryDAL, CategoryDAL>();
